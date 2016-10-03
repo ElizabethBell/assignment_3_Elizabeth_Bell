3ed8bc30639f6c1514835cf384db0bd0f395d7fb
Elizabeth Bell <ebell13@uncc.edu>
2016-10-03 02:34:13 +0000

    updated loop scheduler

--- 1.2/loop_scheduler.c
+++ 1.2/loop_scheduler.c
@@ -5,51 +5,75 @@
 #include <math.h>
 #include <pthread.h>
 
-pthread_mutex_t mut;
+pthread_mutex_t* mut;
 
-int sol = 0;
+double sol = 0;
+int a, b;
 
-void *i() {
-  
-  return NULL;
-}
-
-/*double f (double n, int x) {
+double f (double n, int x) {
   double y=n;
   int i;
   for (i=0; i<x; i++) {
-    y += pow(n, 2.0);
-    //y = sqrt(y);
+    y = pow(1, 2.0);
+    y = sqrt(1);
   }
   return y;
 }
 
-double integrate (int a, int b, int n, int x) {
+double integrate (int a, int b, int x) {
   int i;
-  double sol;
+  double solu;
   struct timeval start, end;
   
   gettimeofday(&start, NULL);
-  for (i=0; i<n; i++) {
-    double num = a+(i+0.5)*((b-a)/n);
-    sol += f(num, x)*((b-a)/n);
+  for (i=0; i<100; i++) {
+    double num = a+(i+0.5)*((b-a)/100);
+    solu += f(num, x)*((b-a)/100);
   }
   gettimeofday(&end, NULL);
   printf("Time: %ld\n", ((end.tv_sec * 1000000 + end.tv_usec)-(start.tv_sec * 1000000 + start.tv_usec)));
-  return sol;
+  return (double)solu;
+}
+
+void* j(int n[]) {
+  int nums[4];
+  int i;
+  for(i=0; i<4; i++) {
+    nums[i]=n[i];
+  }
+  while (b < nums[2]) {
+   pthread_mutex_lock(mut);
+   sol += integrate(a, b, nums[3]);
+   a = b+1;
+   b += nums[3];
+   pthread_mutex_unlock(mut);
+  }
+  pthread_exit(NULL);
+  return NULL;
 }
 
 int main(int argc, char *argv[]) {
-  if(argc != 5) {
+  if(argc != 4) {
     printf("Incorrect number of arguments.\n");
   }
   else {
-    int arg1, arg2, arg3, arg4;
-    arg1 = atoi(argv[1]);
-    arg2 = atoi(argv[2]);
-    arg3 = atoi(argv[3]);
-    arg4 = atoi(argv[4]);
-    integrate(arg1, arg2, arg3, arg4);
+    int numthreads, granularity, intensity, num, i;
+    numthreads = atoi(argv[1]);
+    granularity = atoi(argv[2]);
+    intensity = atoi(argv[3]);
+    num = granularity/numthreads;
+    int nums[4] = {numthreads, granularity, intensity, num};
+    a = 1;
+    b = num;
+    pthread_t th[numthreads];
+    
+    for (i=0; i<numthreads; i++) {
+      pthread_create(&th[i], NULL, j, nums);
+    }
+    for (i=0; i<numthreads; i++) {
+      pthread_join(th[i], NULL);
+    }
   }
+  pthread_exit(NULL);
   return 0;
-}*/
+}
