9c7328aa47968e6e04be470885a4f20f32f51cd9
Elizabeth Bell <ebell13@uncc.edu>
2016-10-03 20:24:19 +0000

    completed 1.2 code

--- 1.2/loop_scheduler.c
+++ 1.2/loop_scheduler.c
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <pthread.h>
 
-pthread_mutex_t* mut;
+pthread_mutex_t mut;
 
 double sol = 0;
 int a, b;
@@ -23,30 +23,36 @@ double f (double n, int x) {
 double integrate (int a, int b, int x) {
   int i;
   double solu;
-  struct timeval start, end;
   
-  gettimeofday(&start, NULL);
   for (i=0; i<100; i++) {
     double num = a+(i+0.5)*((b-a)/100);
     solu += f(num, x)*((b-a)/100);
   }
-  gettimeofday(&end, NULL);
-  printf("Time: %ld\n", ((end.tv_sec * 1000000 + end.tv_usec)-(start.tv_sec * 1000000 + start.tv_usec)));
+  
   return (double)solu;
 }
 
-void* j(int n[]) {
+void* j(void* n) {
   int nums[4];
   int i;
   for(i=0; i<4; i++) {
-    nums[i]=n[i];
+    nums[i]=((int *)n)[i];
   }
-  while (b < nums[2]) {
-   pthread_mutex_lock(mut);
+  while ((b-a) < nums[2]) {
+   pthread_mutex_lock(&mut);
    sol += integrate(a, b, nums[3]);
+   if(b==100) {
+     pthread_mutex_unlock(&mut);
+     break;
+   }
    a = b+1;
-   b += nums[3];
-   pthread_mutex_unlock(mut);
+   if(b+nums[3] > 100) {
+     b = 100;
+   }
+   else {
+     b += nums[3];
+   }
+   pthread_mutex_unlock(&mut);
   }
   pthread_exit(NULL);
   return NULL;
@@ -66,13 +72,18 @@ int main(int argc, char *argv[]) {
     a = 1;
     b = num;
     pthread_t th[numthreads];
+    struct timeval start, end;
+  
+    gettimeofday(&start, NULL);
     
     for (i=0; i<numthreads; i++) {
-      pthread_create(&th[i], NULL, j, nums);
+      pthread_create(th+i, NULL, j, nums);
     }
     for (i=0; i<numthreads; i++) {
       pthread_join(th[i], NULL);
     }
+    gettimeofday(&end, NULL);
+    printf("Time: %ld\n", ((end.tv_sec * 1000000 + end.tv_usec)-(start.tv_sec * 1000000 + start.tv_usec)));
   }
   pthread_exit(NULL);
   return 0;
